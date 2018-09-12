c185815c3f3a2393d6a48d6bf995e04363464876
Oshit <OA3IC.QV@gmail.com>
2018-09-12 16:54:45 +0300

    Add url in students, groups, journal

--- students/views.py
+++ students/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# Students List
+def students_list(request):
+    return render(request, 'students/students_listing.html', {})
+
+def students_add(request):
+    return HttpResponse('<h1>Add Students</h1>')
+
+def students_edit(request):
+    return HttpResponse('<h1>Edit Students</h1>')
+
+def students_delete(request):
+    return HttpResponse('<h1>Delete students</h1>')
+
+# Groups
+def groups_list(request):
+    return render(request, 'students/groups_listing.html', {})
+
+def groups_add(request):
+    return HttpResponse('<h1>Groups Add</h1>')
+
+def groups_edit(request):
+    return HttpResponse('<h1>Groups Edit</h1>')
+
+def groups_delete(request):
+    return HttpResponse('<h1>Groups Delete</h1>')
+
+# Journal
+def journal_list(request):
+    return render(request, 'students/journal_listing.html', {})
+
+def journal_student(request):
+    return HttpResponse('<h1>Journal student</h1>')
+
+def journal_update(request):
+    return HttpResponse('<h1>Journal update</h1>')
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -7,4 +7,21 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # Students List
+    url(r'^$', 'students.views.students_list', name='home'),
+    url(r'^students/add/$', 'students.views.students_add', name='add'),
+    url(r'^students/[0-9]+/edit/$', 'students.views.students_edit', name='edit_students'),
+    url(r'^students/[0-9]+/delete/$', 'students.views.students_delete', name='delete_students'),
+
+    # Groups
+    url(r'^groups/$', 'students.views.groups_list', name='groups'),
+    url(r'^groups/add/$', 'students.views.groups_add', name='add_groups'),
+    url(r'^groups/[0-9]+/edit/$', 'students.views.groups_edit', name='edit_groups'),
+    url(r'groups/[0-9]+/delete/$', 'students.views.groups_delete', name='delete groups'),
+
+    # Journal
+    url(r'^journal/$', 'students.views.journal_list', name='journal'),
+    url(r'^journal/[0-9]+/$', 'students.views.journal_student', name='student_journal'),
+    url(r'^journal/update/$', 'students.views.journal_update', name='update_journal'),
 )
