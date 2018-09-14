9e85881f5e586bfb70577cdb27425eeaddfb9313
Oshit <OA3IC.QV@gmail.com>
2018-09-14 10:05:40 +0300

    We will dynamify the URL of the address

--- students/views.py
+++ students/views.py
@@ -11,11 +11,11 @@ def students_list(request):
 def students_add(request):
     return HttpResponse('<h1>Add Students</h1>')
 
-def students_edit(request):
-    return HttpResponse('<h1>Edit Students</h1>')
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Students %s </h1>' % sid)
 
-def students_delete(request):
-    return HttpResponse('<h1>Delete students</h1>')
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete students %s</h1>' % sid)
 
 # Groups
 def groups_list(request):
@@ -24,18 +24,18 @@ def groups_list(request):
 def groups_add(request):
     return HttpResponse('<h1>Groups Add</h1>')
 
-def groups_edit(request):
-    return HttpResponse('<h1>Groups Edit</h1>')
+def groups_edit(request, sid):
+    return HttpResponse('<h1>Groups Edit</h1 %s>' % sid)
 
-def groups_delete(request):
-    return HttpResponse('<h1>Groups Delete</h1>')
+def groups_delete(request, sid):
+    return HttpResponse('<h1>Groups Delete</h1> %sid </h1>' % sid)
 
 # Journal
 def journal_list(request):
     return render(request, 'students/journal_listing.html', {})
 
-def journal_student(request):
-    return HttpResponse('<h1>Journal student</h1>')
+def journal_student(request, sid):
+    return HttpResponse('<h1>Journal student %s </h1>' % sid)
 
 def journal_update(request):
     return HttpResponse('<h1>Journal update</h1>')
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -10,18 +10,18 @@ urlpatterns = patterns('',
 
     # Students List
     url(r'^$', 'students.views.students_list', name='home'),
-    url(r'^students/add/$', 'students.views.students_add', name='add'),
-    url(r'^students/[0-9]+/edit/$', 'students.views.students_edit', name='edit_students'),
-    url(r'^students/[0-9]+/delete/$', 'students.views.students_delete', name='delete_students'),
+    url(r'^students/add/$', 'students.views.students_add', name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', 'students.views.students_edit', name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete', name='students_delete'),
 
     # Groups
     url(r'^groups/$', 'students.views.groups_list', name='groups'),
     url(r'^groups/add/$', 'students.views.groups_add', name='add_groups'),
-    url(r'^groups/[0-9]+/edit/$', 'students.views.groups_edit', name='edit_groups'),
-    url(r'groups/[0-9]+/delete/$', 'students.views.groups_delete', name='delete groups'),
+    url(r'^groups/(?P<sid>\d+)/edit/$', 'students.views.groups_edit', name='edit_groups'),
+    url(r'groups/(?P<sid>\d+)/delete/$', 'students.views.groups_delete', name='delete groups'),
 
     # Journal
     url(r'^journal/$', 'students.views.journal_list', name='journal'),
-    url(r'^journal/[0-9]+/$', 'students.views.journal_student', name='student_journal'),
+    url(r'^journal/(?P<sid>\d+)/$', 'students.views.journal_student', name='student_journal'),
     url(r'^journal/update/$', 'students.views.journal_update', name='update_journal'),
 )
