4d461ccd531ccd9324c744df023426dca12b3e08
Oshit <OA3IC.QV@gmail.com>
2018-09-15 18:00:09 +0300

    Plugged in dynamic templates

--- students/views.py
+++ students/views.py
@@ -1,12 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 
-# Students List
+# Views for Students
 def students_list(request):
-    return render(request, 'students/students_listing.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Подоба',
+         'last_name': u'Віталій',
+         'ticket': 235,
+         'image': 'img/1.jpeg'},
+        {'id': 2,
+         'first_name': u'Корст',
+         'last_name': u'Андрій',
+         'ticket': 2123,
+         'image': 'img/2.jpg'},
+        {'id': 3,
+         'first_name': u'Притула',
+         'last_name': u'Тарас',
+         'ticket': 5000,
+         'image': 'img/3.jpg'},
+    )
+
+    groups = (
+        {'id': 'МтМ-21',
+         'monitor': 'Віталій Подоба'},
+        {'id': 'МтМ-22',
+         'monitor': 'Олег Корст'},
+        {'id': 'Мтм-23',
+         'monitor': 'Іван Іванов'},
+    )
+    return render(request, 'students/students_listing.html', {'students': students, 'groups': groups})
 
 def students_add(request):
     return HttpResponse('<h1>Add Students</h1>')
@@ -19,7 +46,15 @@ def students_delete(request, sid):
 
 # Groups
 def groups_list(request):
-    return render(request, 'students/groups_listing.html', {})
+    groups = (
+        {'id': 'МтМ-21',
+         'monitor': 'Віталій Подоба'},
+        {'id': 'МтМ-22',
+         'monitor': 'Олег Корст'},
+        {'id': 'Мтм-23',
+         'monitor': 'Іван Іванов'},
+    )
+    return render(request, 'students/groups_listing.html', {'groups': groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Groups Add</h1>')
@@ -32,7 +67,15 @@ def groups_delete(request, sid):
 
 # Journal
 def journal_list(request):
-    return render(request, 'students/journal_listing.html', {})
+    groups = (
+        {'id': 'МтМ-21',
+         'monitor': 'Віталій Подоба'},
+        {'id': 'МтМ-22',
+         'monitor': 'Олег Корст'},
+        {'id': 'Мтм-23',
+         'monitor': 'Іван Іванов'},
+    )
+    return render(request, 'students/journal_listing.html', {'groups': groups})
 
 def journal_student(request, sid):
     return HttpResponse('<h1>Journal student %s </h1>' % sid)
