10498d33870c74ae50d02bb8f0bc92a928955e64
Oshit <OA3IC.QV@gmail.com>
2018-09-17 14:47:47 +0300

    Create models group

--- students/views/students.py
+++ students/views/students.py
@@ -1,36 +1,34 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from ..models import Student, Group
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Views for Students
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Подоба',
-         'last_name': u'Віталій',
-         'ticket': 235,
-         'image': 'img/1.jpeg'},
-        {'id': 2,
-         'first_name': u'Корст',
-         'last_name': u'Андрій',
-         'ticket': 2123,
-         'image': 'img/2.jpg'},
-        {'id': 3,
-         'first_name': u'Притула',
-         'last_name': u'Тарас',
-         'ticket': 5000,
-         'image': 'img/3.jpg'},
-    )
+    students = Student.objects.all()
 
-    groups = (
-        {'id': 'МтМ-21',
-         'monitor': 'Віталій Подоба'},
-        {'id': 'МтМ-22',
-         'monitor': 'Олег Корст'},
-        {'id': 'Мтм-23',
-         'monitor': 'Іван Іванов'},
-    )
+    # try to order students list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('last_name', 'first_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results
+        students = paginator.page(paginator.num_pages)
+
+    groups = Group.objects.all()
     return render(request, 'students/students_listing.html', {'students': students, 'groups': groups})
 
 def students_add(request):
